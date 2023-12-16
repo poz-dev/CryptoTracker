0d35aae38a6c74f18c929b4d9db8110d823681aa
poz-dev <kresimir.ivanjko@icloud.com>
2023-12-16 11:36:57 +0100

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Kresimir Ivanjko on 16.12.2023..
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
